27daffa3b5c37fa1e77132e84a7bdf01034dd690
TMM\135670 <Vijayaraagavan.Manoharan@toyota.com>
2017-12-18 22:23:21 -0600

    Improved File Creation Date Efficiency.

--- src/main/java/poc/vijay/mediaOrganizer/App.java
+++ src/main/java/poc/vijay/mediaOrganizer/App.java
@@ -9,8 +9,10 @@ import org.apache.tika.exception.TikaException;
 import org.xml.sax.SAXException;
 
 /**
- * MediaNameFormatter - Class to rename the files as yyyyMMdd_HHmmss format by when the picture/video taken.
- * @author Vijay 
+ * MediaNameFormatter - Class to rename the files as yyyyMMdd_HHmmss format by
+ * when the picture/video taken.
+ * 
+ * @author Vijay
  *
  */
 public class App {
@@ -19,44 +21,53 @@ public class App {
 
 	public static void main(String[] args) {
 
-		TikaParser tikaParser = new TikaParser();
 		try {
 
-			File root = new File("C:\\Vijay\\media");
+			if (args.length > 0 && args[0] != null) {
 
-			if (root.isDirectory()) {
-				File[] fileArray = root.listFiles();
-				if (fileArray != null && fileArray.length > 0) {
-					int fileCount = 1;
-					List<String> notModifiedFiles = new ArrayList<String>();
-					for (File file : fileArray) {
-						if (file.isFile()) {
-							String creationDate = tikaParser.fetchCreationDate(file.getAbsolutePath());
-							if (creationDate != null && !creationDate.isEmpty()) {
-								String extension = getExtension(file.getName());
-								File newFile = new File(root + PATH_SEPARATOR + creationDate + extension);
-								if (newFile.exists()) {
-									Integer count = Integer.valueOf(0);
-									newFile = fetchNextUniqueFileObject(root.getPath(), creationDate, extension, count);
-								}
-								System.out.println(fileCount + ". Old File Name :: " + file.getName()
-										+ " ;New File Name :: " + newFile.getName());
-								boolean success = file.renameTo(newFile);
-								if (!success) {
-									notModifiedFiles.add(file.getName());
-								}
+				File root = new File(args[0]);
 
+				if (root.isDirectory()) {
+					File[] fileArray = root.listFiles();
+					if (fileArray != null && fileArray.length > 0) {
+						int fileCount = 1;
+						List<String> notModifiedFiles = new ArrayList<String>();
+						for (File file : fileArray) {
+							
+							if (file.isFile()) {
+								String creationDate = fetchCreationDate(file.getAbsolutePath());
+
+								if (creationDate != null && !creationDate.isEmpty()) {
+									String extension = getExtension(file.getName());
+									File newFile = new File(root + PATH_SEPARATOR + creationDate + extension);
+									if (newFile.exists()) {
+										Integer count = Integer.valueOf(0);
+										newFile = fetchNextUniqueFileObject(root.getPath(), creationDate, extension,
+												count);
+									}
+									System.out.println(fileCount + ". Old File Name :: " + file.getName()
+											+ " ;New File Name :: " + newFile.getName());
+									boolean success = file.renameTo(newFile);
+									if (!success) {
+										notModifiedFiles.add(file.getName());
+									}
+
+								}
 							}
+							fileCount = fileCount + 1;
 						}
-						fileCount = fileCount + 1;
-					}
 
-					if (notModifiedFiles != null && !notModifiedFiles.isEmpty()) {
-						for (String fileName : notModifiedFiles) {
-							System.err.println(fileName + " is not modified");
+						if (notModifiedFiles != null && !notModifiedFiles.isEmpty()) {
+							for (String fileName : notModifiedFiles) {
+								System.err.println(fileName + " is not modified");
+							}
 						}
 					}
 				}
+			}
+
+			else {
+				System.out.println("There is no Argument with valid file path");
 			}
 
 		} catch (IOException e) {
@@ -72,7 +83,28 @@ public class App {
 	}
 
 	/**
-	 * Method to get next Unique File Object. 
+	 * Method to fetch best FileCreationDate.
+	 * 
+	 * @param absolutePath as String
+	 * @return creationDate as String
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws TikaException
+	 */
+	private static String fetchCreationDate(String absolutePath) throws IOException, SAXException, TikaException {
+		String creationDate = null;
+		if (absolutePath != null) {
+			creationDate = TikaParserService.getInstance().fetchCreationDate(absolutePath);
+			if (creationDate == null || creationDate.isEmpty()) {
+				creationDate = FileAttributesService.getInstance().fetchCreationDate(absolutePath);
+			}
+		}
+		return creationDate;
+	}
+
+	/**
+	 * Method to get next Unique File Object.
+	 * 
 	 * @param path as String
 	 * @param creationDate as String
 	 * @param extension as String
@@ -92,6 +124,7 @@ public class App {
 
 	/**
 	 * Method to get extension for a file.
+	 * 
 	 * @param name as String
 	 * @return extension as String
 	 */
@@ -101,4 +134,5 @@ public class App {
 		extension = name.substring(lastIndex);
 		return extension;
 	}
+
 }
